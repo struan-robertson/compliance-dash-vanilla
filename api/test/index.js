10092df05b8b8448f3baf5ba03357e7cf9264713
Struan Robertson <struanrobertson@protonmail.com>
2022-10-09 18:14:18 +0100

    test 1000

--- api/test/index.js
+++ api/test/index.js
@@ -1,13 +1,13 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const randomstring = require('randomstring');
-const sql = require('mssql');
+// const jwt = require('jsonwebtoken');
+// const bcrypt = require('bcrypt');
+// const randomstring = require('randomstring');
+// const sql = require('mssql');
 
-module.exports = async function (context, req, process) {
+module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
 
-    const test = process.env["test"];
+    const test = "AzureWebJobsStorage: " + process.env["APPINSIGHTS_INSTRUMENTATIONKEY"];
 
     context.res = {
         // status: 200, /* Defaults to 200 */
