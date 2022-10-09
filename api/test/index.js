342de6407c24bf2aa573fcfe1c6ee735a12d7caf
Struan Robertson <struanrobertson@protonmail.com>
2022-10-09 18:02:57 +0100

    if this doesnt work soon then hardcode values

--- api/test/index.js
+++ api/test/index.js
@@ -7,8 +7,10 @@ module.exports = async function (context, req, process) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
 
+    const test = process.env["test"];
+
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: process.env.test
+        body: test
     };
 }
