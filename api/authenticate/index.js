e2da741a818d386c628577eba18a505796f4801e
Struan Robertson <struanrobertson@protonmail.com>
2022-10-09 18:45:08 +0100

    return error

--- api/authenticate/index.js
+++ api/authenticate/index.js
@@ -25,7 +25,7 @@ module.exports = async function (context, req) {
     {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: passwordQuery
+            body: err
         };
     }
 
