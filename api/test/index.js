e852596bf73d0f5c9b2186bc8509c1824711a886
Struan Robertson <struanrobertson@protonmail.com>
2022-10-09 17:24:57 +0100

    test env vars

--- api/test/index.js
+++ api/test/index.js
@@ -6,13 +6,9 @@ const sql = require('mssql');
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: process.env["TEST_DATABASE_CONNECTION_STRING"]
     };
 }
