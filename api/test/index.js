f352d4e70cf48619c30a7ac7920527f0b90e05aa
Struan Robertson <struanrobertson@protonmail.com>
2022-10-09 17:22:27 +0100

    test imports

--- api/test/index.js
+++ api/test/index.js
@@ -1,3 +1,8 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const randomstring = require('randomstring');
+const sql = require('mssql');
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
