aa4e83bbabe297eb5612877b38531e3877182aaf
Struan Robertson <struanrobertson@protonmail.com>
2022-10-09 17:59:16 +0100

    pass process as a parameter

--- api/test/index.js
+++ api/test/index.js
@@ -3,12 +3,12 @@ const bcrypt = require('bcrypt');
 const randomstring = require('randomstring');
 const sql = require('mssql');
 
-module.exports = async function (context, req) {
+module.exports = async function (context, req, process) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: process.env
+        body: process.env.test
     };
 }
